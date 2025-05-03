02bd575538cbfe35c8614a563eb3550445abe47e
AlokSreenivasan <aloksreenivasank@gmail.com>
2025-05-03 12:02:09 +0530

    Implemented Maps in Android

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -11,7 +11,7 @@ function AppNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Signup"
+        initialRouteName="Geofence"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="About" component={About} />
--- src/screens/Geofence.tsx
+++ src/screens/Geofence.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import MapView from 'react-native-maps';
 
 function Geofence() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <Text>Geofence</Text>
+      <MapView style={styles.map}></MapView>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 24,
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default Geofence;
